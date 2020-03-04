14ef196e2c5d0c9397bdcd34e18f1516d19654cc
charlyecastro <castroc@uw.edu>
2020-03-04 10:55:14 -0800

    storing recipe data on mongodb

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
 // Put interpreter into strict mode
 "use strict";
 
-// Imports
+// Imports 
 const express = require('express');
 const multer = require('multer');
 const morgan = require("morgan");
@@ -9,31 +9,11 @@ const bodyParser = require("body-parser")
 const ejs = require('ejs');
 const path = require('path');
 const mongoose = require("mongoose");
-
-// Fetch environment variables
-const addr = process.env.MESSAGESADDR || ":80";
-const mongoAddr = process.env.MONGOADDR;
-const [host, port] = addr.split(":");
-
-// Set up mongo url and connect to database
-const url = "mongodb://" + mongoAddr + "/cookbook"
-
-mongoose.connect(url, { useNewUrlParser: true })
-    .then(() => { console.log("Connected to database!") })
-    .catch((err) => { console.log("couldnt connect to database because of ERR: ", err) })
-
-// Init app and use middlewares
-const app = express();
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const Recipe = require("./models/recipe")
 
 // Set multer storage
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
+    destination: './uploads/',
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() +
         path.extname(file.originalname));
@@ -43,10 +23,33 @@ const storage = multer.diskStorage({
 // Init multer uploads
 const upload = multer({
     storage: storage,
+    limits: 1024 * 1024 * 5,
     fileFilter: function(req ,file ,cb) {
         checkFileType(file, cb);
     }
-}).single('foodImage');
+})
+//.single('foodImage');
+
+// Fetch environment variables
+const addr = process.env.MESSAGESADDR || ":80";
+const mongoAddr = process.env.MONGOADDR;
+const [host, port] = addr.split(":");
+
+// Set up mongo url and connect to database
+const url = "mongodb://" + mongoAddr + "/cookbook"
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => { console.log("Connected to database!") })
+    .catch((err) => { console.log("couldnt connect to database because of ERR: ", err) })
+
+// Init app and use middlewares
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/uploads', express.static('uploads'));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Check file type
 function checkFileType(file, cb) {
@@ -70,17 +73,24 @@ function checkFileType(file, cb) {
 app.get('/', (req, res) => res.render('index'))
 
 // Upload route
-app.post('/upload', (req, res) => {
-    upload(req,res, (err) => {
-        if (err) {
-            res.render('index', {
-                msg: err
-            })
-        } else {
-            console.log(req.file);
-            res.send('test')
-        }
-    });
+app.post('/upload',upload.single('foodImg'), (req, res) => {
+    const recipe = new Recipe({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        description: req.body.description,
+        author: req.body.author,
+        foodImg: req.file.path
+    })
+    recipe.save()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    res.status(201).json({
+        recipe
+    })
 });
 
 // Server listening
---
+++ Server/models/recipe.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required : true},
+    description: {type: String, required : true},
+    author: {type: String, required : true},
+    private: { type: Boolean, default: false },
+    members: [],
+    foodImg: {type: String, required : true},
+    categoryList: [String],
+    createdAt: { type: Date, default: Date.now },
+    editedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
